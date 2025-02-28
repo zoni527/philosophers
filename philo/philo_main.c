c1930c63247eaefa426150910a59970c6dffdf51
Johnny Varila <jvarila@student.hive.fi>
2025-02-28 14:44:17 +0200

    Removed the monitoring function as philosophers manage on their own without it just fine.

--- philo/philo_main.c
+++ philo/philo_main.c
@@ -12,9 +12,6 @@
 
 #include "philo.h"
 
-static void	observe_the_party(t_data *data);
-static bool	philo_has_eaten_enough(t_philo *philo);
-
 int	main(int argc, char *argv[])
 {
 	static t_data	data;
@@ -35,49 +32,8 @@ int	main(int argc, char *argv[])
 		destroy_mutexes(&data);
 		return (cleanup_and_return(&data, E_THREAD));
 	}
-	observe_the_party(&data);
 	merge_threads(&data);
 	destroy_mutexes(&data);
 	cleanup_data(&data);
 	return (EXIT_SUCCESS);
-}
-
-static void	observe_the_party(t_data *data)
-{
-	unsigned int	i;
-	unsigned int	full_philos;
-
-	wait_till_start(&data->start_time);
-	while (party_still_going(&data->philos[0]))
-	{
-		full_philos = 0;
-		i = 0;
-		while (i < data->n_philos)
-		{
-			if (should_be_dead(&data->philos[i]) \
-				&& !philo_has_eaten_enough(&data->philos[i]))
-			{
-				die_and_stop_the_party(&data->philos[i]);
-				break ;
-			}
-			if (philo_has_eaten_enough(&data->philos[i]))
-				full_philos++;
-			i++;
-		}
-		if (full_philos == data->n_philos)
-			break ;
-	}
-}
-
-static bool	philo_has_eaten_enough(t_philo *philo)
-{
-	bool	cant_eat_more;
-
-	pthread_mutex_lock(philo->sim_lock);
-	if (philo->meals_eaten >= *philo->n_meals && *philo->n_meals != 0)
-		cant_eat_more = true;
-	else
-		cant_eat_more = false;
-	pthread_mutex_unlock(philo->sim_lock);
-	return (cant_eat_more);
 }
